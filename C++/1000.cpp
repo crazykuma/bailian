242b9237226ed2be4929ef54192a8ca47a802c0a
crazykuma <crazykuma@qq.com>
2019-11-16 16:28:25 +0800

    1000:A+B Problem

---
+++ C++/1000.cpp
@@ -0,0 +1,23 @@
+/* 1000:A+B Problem
+查看提交统计提示提问
+总时间限制: 1000ms 内存限制: 65536kB
+描述
+Calculate a + b
+
+输入
+Two integer a,,b (0 ≤ a,b ≤ 10)
+输出
+Output a + b
+样例输入
+1 2
+样例输出
+3 */
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    return 0;
+}
